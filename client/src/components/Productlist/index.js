831e2e55c5700563db36617686d02310ab181e42
Carlie Smith <carliesmith011@gmail.com>
2022-05-04 20:46:15 -0400

    starting pages and component tie-ins

--- client/src/components/Productlist/index.js
+++ client/src/components/Productlist/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SingleProduct from '../../pages/SingleProduct';
 
 const Productlist = ({ products, title }) => {
-  // if (!thoughts.length) {
-  //   return <h3>No Thoughts Yet</h3>;
-  // }
+
 
   return (
     <div>
@@ -13,26 +12,17 @@ const Productlist = ({ products, title }) => {
         products.map(product => (
           <div key={product._id} className="card mb-3">
             <p className="card-header">
-              <Link to={`/profile/${product}`}
-              style={{ fontWeight: 700 }}
-              className="text-light">
-                {product}
-                </Link>{' '}
-                thought on {product.createdAt}
-            </p>
-            <div className="card-body">
-              <Link to={`/product/${product._id}`}>
-              <p>{product.description}</p>
-              <p className="mb-0">
-                Comments: {product.commentCount} || Click to{' '}
-                {product.commentCount ? 'see' : 'start'} share your thoughts!
-              </p>
-              </Link>
-            </div>
+              
+              <img src='../../images/${product.image}'/>
+            
+               </p>
+            
           </div>
         ))}
     </div>
-  );
-};
+    );
+        };
+        
 
 export default Productlist;
+
